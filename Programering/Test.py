f696c7b4f1f1631c9e839ace49169d5616057660
FransKarlsson <70134935+FransKarlsson@users.noreply.github.com>
2020-11-18 12:21:40 +0100

    e
    e

--- Programering/Test.py
+++ Programering/Test.py
@@ -1,1 +1,2 @@
-print("Hej")
+for i in range(1,10):
+    print("Hej")
---
+++ Programering/strängar.py
@@ -0,0 +1,6 @@
+#upg 1
+'''
+namn=str(input("Skriv in ditt namn."))
+print(f"{namn} inehåller {len(namn)} bokstäver.")
+'''
+print("hej")
